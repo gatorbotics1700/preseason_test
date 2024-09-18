9737003167ab22fb2b0a17345d66df8a218195b5
Selin Aytekin <selinaytekinca@Selins-MacBook-Air.local>
2024-09-17 17:55:16 -0700

    Hello

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+    public static void helloWorld(){
+        System.out.println("hello world!");
+    }
+    public static void main(String args[]){
+        helloWorld();
+    }
+}
