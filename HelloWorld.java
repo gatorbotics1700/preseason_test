c9895caa88716af34ff555c6f9ca37820a5f7f61
Iyla Parikh <iylaparikh@Iylas-MacBook-Pro.local>
2024-09-17 17:48:52 -0700

    intro to java workshop HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void helloWorld(){
+        System.out.println("hello world");
+    }
+    public static void main(String args[]){
+        helloWorld();
+    }
+}
