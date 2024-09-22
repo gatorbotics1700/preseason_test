6cd426fd604244a584c8ad77639ed1d1dafe739f
Camille Yung <28cyung@castilleja.org>
2024-09-22 14:39:41 -0700

    camille's intro software test

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    
+    public static void main(String[]args){
+        helloWorld();
+    }
+
+    public static void helloWorld(){
+        System.out.println("Hello World!");
+    }
+}
